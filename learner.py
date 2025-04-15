d86c75c54fa0a06d6f2158c703227ca31e118ade
luoym-213 <lym18019092136@163.com>
2025-04-15 09:27:32 +0800

    dev:添加对其他智能体观测的mask计算

--- learner.py
+++ learner.py
@@ -59,7 +59,7 @@ def setup_master(args, env=None, return_env=False):
         else:
             if policy2 is None:
                 policy2 = MPNN(input_size=pol_obs_dim,num_agents=num_friendly,num_entities=num_entities,action_space=action_space,
-                               pos_index=pos_index, mask_dist=args.mask_dist,entity_mp=entity_mp).to(args.device)
+                               pos_index=pos_index, mask_dist=args.mask_dist,mask_obs_dist=args.mask_obs_dist,entity_mp=entity_mp).to(args.device)
             team2.append(Neo(args,policy2,(obs_dim,),action_space))
     master = Learner(args, [team1, team2], [policy1, policy2], env)
     
